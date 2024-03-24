45988e11162b2639b861d9cd980d88f4f2dd61d4
Thomas Grbic <tomgrbz@gmail.com>
2024-03-23 23:51:07 -0400

    Fixed no products avail screeN

--- app/src/main/java/com/cs4520/assignment5/ui/ProductListScreen.kt
+++ app/src/main/java/com/cs4520/assignment5/ui/ProductListScreen.kt
@@ -1,5 +1,8 @@
 package com.cs4520.assignment5.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -9,6 +12,11 @@ import androidx.lifecycle.viewmodel.viewModelFactory
 import com.cs4520.assignment5.data_layer.ProductRepository
 import com.cs4520.assignment5.view_model.ProductListViewModel
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.cs4520.assignment5.application.ProductApplication
 
 @Composable
@@ -16,10 +24,18 @@ fun ProductListScreen(productRepo: ProductRepository) {
     val viewModel: ProductListViewModel = viewModel(factory = ProductListViewModel.Factory)
     val products by viewModel.products.collectAsState()
 
-
-    LazyColumn {
-        items(products) { product ->
-            ProductRow(product)
+    if (products.isEmpty()) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "No Products Available", fontSize = 18.sp)
+        }
+    } else {
+        LazyColumn {
+            items(products) { product ->
+                ProductRow(product)
+            }
         }
     }
 }
